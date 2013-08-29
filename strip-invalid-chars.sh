08e606b54904df565128c63cb36a80a5c8ff2b95
Jeremy O'Brien <obrien654j@gmail.com>
2013-08-29 10:25:18 -0400

    add an extra char to replace in the replacement script

--- strip-invalid-chars.sh
+++ strip-invalid-chars.sh
@@ -1,1 +1,1 @@
-cat "$1" |sed -e 's/\x96//g;s/\x93/"/g;s/\x94/"/g;s/\x92/\x27/g;s/\xa0//g' >"$1-fixed"
+cat "$1" |sed -e 's/\x96//g;s/\x93/"/g;s/\x94/"/g;s/\x92/\x27/g;s/\xa0//g;s/\xff/\//g' >"$1-fixed"
